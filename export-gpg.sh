bd707ce89ff808dc02c43318c884a8e43063c0cc
mfish38 <markdfisher38@gmail.com>
2026-03-05 11:22:10 -0500

    added empty check

--- export-gpg.sh
+++ export-gpg.sh
@@ -1,4 +1,7 @@
 mkdir gpg-export
 gpg --export-secret-keys --armor > gpg-export/private-keys.gpg
+[ -s gpg-export/private-keys.gpg ] || echo "Error: gpg-export/private-keys.gpg is empty" >&2
 gpg --export --armor > gpg-export/public-keys.gpg
+[ -s gpg-export/public-keys.gpg ] || echo "Error: gpg-export/public-keys.gpg is empty" >&2
 gpg --export-ownertrust > gpg-export/trustdb.txt
+[ -s gpg-export/trustdb.txt ] || echo "Error: gpg-export/trustdb.txt is empty" >&2
